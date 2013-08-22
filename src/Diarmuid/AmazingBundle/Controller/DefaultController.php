1f4f9bd70bcd750862c18e3fc363e3d13ff61fba
diarmuid <contact@diarmuid.ie>
2013-08-22 19:48:35 +0100

    Complete CRUD Bundle
    Basic CRUD bundle using Doctrine and Symfony Forms.

--- src/Diarmuid/AmazingBundle/Controller/DefaultController.php
+++ src/Diarmuid/AmazingBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Diarmuid\AmazingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Diarmuid\AmazingBundle\Entity\Link;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -12,12 +14,6 @@ class DefaultController extends Controller
         $links = $this->getDoctrine()
             ->getRepository('DiarmuidAmazingBundle:Link')
             ->findAll();
-
-        if (!$links) {
-            throw $this->createNotFoundException(
-                'No Links Found'
-            );
-        }
 
         return $this->render('DiarmuidAmazingBundle:Default:index.html.twig', array('links' => $links));
     }
@@ -37,15 +33,87 @@ class DefaultController extends Controller
         return $this->render('DiarmuidAmazingBundle:Default:show.html.twig', array("link" => $link));
     }
 
-    public function addAction() {
-        return $this->render('DiarmuidAmazingBundle:Default:index.html.twig');
+    public function addAction(Request $request) {
+
+        $link = new Link();
+
+        $form = $this->createFormBuilder($link)
+            ->add('URL', 'url')
+            ->add('Title', 'textarea')
+            ->add('save', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($link);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl(
+                'Link_show',
+                array('id' => $link->getId()))
+            );
+        }
+
+        return $this->render('DiarmuidAmazingBundle:Default:add.html.twig', array('form' => $form->createView()) );
     }
 
-    public function deleteAction() {
-        return $this->render('DiarmuidAmazingBundle:Default:index.html.twig');
+    public function deleteAction($id) {
+
+        $link = $this->getDoctrine()
+            ->getRepository('DiarmuidAmazingBundle:Link')
+            ->findOneById($id);
+
+        if (!$link) {
+            throw $this->createNotFoundException(
+                $id . ' Not Found'
+            );
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($link);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('Link'));
+
     }
 
-    public function updateAction() {
-        return $this->render('DiarmuidAmazingBundle:Default:index.html.twig');
+    public function updateAction(Request $request, $id) {
+
+        $link = $this->getDoctrine()
+            ->getRepository('DiarmuidAmazingBundle:Link')
+            ->findOneById($id);
+
+        if (!$link) {
+            throw $this->createNotFoundException(
+                $id . ' Not Found'
+            );
+        }
+
+        $form = $this->createFormBuilder($link)
+            ->add('URL', 'url')
+            ->add('Title', 'textarea')
+            ->add('save', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($link);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl(
+                    'Link_show',
+                    array('id' => $link->getId()))
+            );
+        }
+
+        return $this->render('DiarmuidAmazingBundle:Default:add.html.twig', array('form' => $form->createView()) );
     }
 }
--- src/Diarmuid/AmazingBundle/README.md
+++ src/Diarmuid/AmazingBundle/README.md
@@ -1,15 +1,13 @@
-Available at http://localhost:8000/app_dev.php/link
 
 Used Doctrine to manage DB connections.
 
-## Methods: ##
+## Methods/Routes: ##
 - Link - shows all links
 - Link/{id} - shows one link
 - Link/add - add an id *
-- Link/delete/{id} - remove a link *
-- Link/update/{id} - update a link *
+- Link/delete/{id} - remove a link
+- Link/update/{id} - update a link
 
-\* Not yet built
 
 ## Install: ##
 This assumes you have a DB connection already setup in app/config/parameters.yml.
@@ -19,3 +17,5 @@ You will need to run:
     php app/console doctrine:schema:update --force
     
 To get Doctrine to build out the tables based on the Entity/link.yml file.
+
+Run server and visit [http://localhost:8000/app_dev.php/link](http://localhost:8000/app_dev.php/link)
